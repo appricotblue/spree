9e3e8aa36267e4653d8de908fb77587686906f72
appricotblue <git2@aindriya.com>
2024-01-25 14:36:17 +0530

    update settings

--- Spree/settings.py
+++ Spree/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static')
 ]
 STATIC_URL = '/static/'
-
+STACTIC_ROOT = os.path.join(BASE_DIR,"static")
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 MEDIA_URL='/media/'
