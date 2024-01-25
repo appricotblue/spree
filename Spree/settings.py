892f6ebb5d477e5008c47ed40c04870c37072b5f
appricotblue <90761953+appricotblue@users.noreply.github.com>
2024-01-25 15:13:01 +0530

    Update settings.py

--- Spree/settings.py
+++ Spree/settings.py
@@ -132,9 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS=[
+# os.path.join(BASE_DIR,'static')
+# ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
